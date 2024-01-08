1abd646ae766a45ff3b78da71cd5fa1a65b9e8b9
Khushi Sinha <khushisinha4786@gmail.com>
2024-01-08 13:03:03 +0530

    Added Find the Winner of the Circular Game

---
+++ Microsoft/find_the_winnner_of_the_circular_game.cpp
@@ -0,0 +1,16 @@
+//leetcode.com/problems/find-the-winner-of-the-circular-game/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int winner = 0;
+        
+        for (int i = 2; i <= n; ++i)
+            winner = (winner + k) % i;
+        
+        return winner + 1;
+    }
+};
